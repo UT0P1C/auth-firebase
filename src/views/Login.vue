509b2aca054c987e73f743a609a2351da5e696c5
UT0P1C <ljonham@gmail.com>
2022-02-07 19:10:29 -0300

    adicionando tela de login e registro

---
+++ src/views/Login.vue
@@ -0,0 +1,103 @@
+<template>
+
+    <div class="login">
+
+        <div class="login-title">
+
+            <h1>Login</h1>
+
+        </div>
+
+        <div class="login-form">
+
+            <input type="email" class="login-form-email" placeholder="E-mail" required>
+
+            <input type="password" class="login-form-password" placeholder="Password" required>
+
+        </div>
+
+        <div class="login-submit">
+
+            <button class="login-button" v-on:click="login">Login</button>
+
+        </div>
+
+        <div class="dont-have">
+            <p>You don't have an account?</p>
+            <a href="/register">Register</a>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name:'Login',
+    data(){
+        return {
+
+        }
+    },
+    methods: {
+        login: function(){
+            this.$router.replace('home');
+        }
+
+    }
+}
+</script>
+
+<style scoped>
+
+    .login {
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+    }
+    .login-form{
+        border-radius: 20px;
+        width: 100px;
+        height: 100px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .login-form-password{
+        margin-top: 20px;
+    }
+
+    .login-button{
+    padding: 10px 15px;
+    background-color: var(--color-background-button);
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: 0.2s opacity;
+    }
+
+    .login-button:hover{
+        opacity: 0.8;
+    }
+
+    .dont-have{
+        margin-top: 20px;
+        text-decoration: none;
+        color: var(--color-text-title);
+    }
+
+    .dont-have a{
+        color: var(--color-text-light);
+        margin-top: 10px;
+    }
+
+    .login-form input{
+        width: 200px;
+        height: 30px;
+        border: none;
+    }
+
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="sign-up">
+        <div class="sign-up-title">
+            <h1>Create Your Account !</h1>
+        </div>
+
+        <div class="sign-up-form">
+
+            <input type="email" class="sign-up-form-email" placeholder="E-mail" required>
+
+            <input type="password" class="sign-up-form-password" placeholder="Password" required>
+
+        </div>
+
+        <div class="sign-up-button">
+
+            <button class="sign-up-button">Sign Up!</button>
+
+        </div>
+
+        <div class="return">
+            <span>Or back to <a href="/login">Login</a></span>
+        </div>
+
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Register'
+}
+</script>
+
+<style scoped>
+    .sign-up {
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+    }
+
+    .sign-up-form{
+        border-radius: 20px;
+        width: 100px;
+        height: 100px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .sign-up-form-password{
+        margin-top: 20px;
+    }
+
+    .sign-up-button{
+        padding: 5px 10px;
+        background-color: var(--color-background-button);
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: 0.2s opacity;
+    }
+
+    .sign-up-button:hover{
+        opacity: 0.8;
+    }
+
+    .return{
+        margin-top: 20px;
+        text-decoration: none;
+        color: var(--color-text-title);
+    }
+
+    .return a{
+        color: var(--color-text-light);
+        margin-top: 10px;
+    }
+
+    .sign-up-form input{
+        width: 200px;
+        height: 30px;
+        border: none;
+    }
+
+</style>
